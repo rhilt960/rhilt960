bcd3283d8cccbcbb0d30058f1ae63cf617dc7b13
rhilt960 <rhilt@samford.edu>
2024-10-16 12:50:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,5 @@ function downloadData() {
   var enddate = document.getElementById("enddate").value;
   console.log("Starting date: " + startdate);
   console.log("Ending date: " + enddate);
+  fetch("https://compute.samford.edu/zohauth/clients/data");
 }
