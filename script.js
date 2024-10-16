c20771e0af8a2960cdb2a4f83f2512260eb00df1
rhilt960 <rhilt@samford.edu>
2024-10-16 12:44:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ function downloadData() {
   event.preventDefault();
   var startdate = document.getElementById("startdate").value;
   var enddate = document.getElementById("enddate").value;
-  console.log("startdate");
+  console.log("Starting date: " + startdate);
   console.log("Ending date: " + enddate);
 }
